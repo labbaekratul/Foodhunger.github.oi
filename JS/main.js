797b7777b6d6e9617b91d99328d82454a3be4872
Labbaek ratul <labbaekratul@gmail.com>
2021-01-27 04:46:37 -0800

    Foodhunger

---
+++ JS/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+
+
+$('.Image_popup .overlay i').mouseover(function(){
+    $(this).hide(1500);
+
+    
+})
+
+$('.Image_popup .overlay i').mouseout(function(){
+    $(this).show(1500);
+
+    
+})
+
+
+    
+
+
+
+
+
+})
